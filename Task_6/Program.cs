aac9a7e8cd60fbfe0ad9935a19665db9c947ecab
VeraBu <opilki-in-yhe-head@mail.ru>
2022-11-13 16:15:36 +0300

    correct Task 6

--- Task_6/Program.cs
+++ Task_6/Program.cs
@@ -4,7 +4,7 @@
 Console.WriteLine("Введите число и нажмите enter");
 string inputA = Console.ReadLine() ?? "";
 double a = Convert.ToDouble(inputA);
-// double res = a%2;
+//double res = a%2;
 if (a%2==0)
 {
     Console.WriteLine($"Число {a} четное");
