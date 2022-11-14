4ab8a185b1a282f628425794649d5383d1ab78cb
VeraBu <opilki-in-yhe-head@mail.ru>
2022-11-14 16:14:49 +0300

    corrected a little in Task4 & Task 6

--- Task_4/Program.cs
+++ Task_4/Program.cs
@@ -1,11 +1,11 @@
 ﻿// Задача 4: Напишите программу, которая принимает на вход три числа и 
 // выдаёт максимальное из этих чисел.
 
-Console.WriteLine("Введите целое число №1 и нажмите enter");
+Console.WriteLine("Введите целое число №1 и нажмите enter: ");
 string InputA = Console.ReadLine() ?? "";
-Console.WriteLine("Введите целое число №2 и нажмите enter");
+Console.WriteLine("Введите целое число №2 и нажмите enter: ");
 string InputB = Console.ReadLine() ?? "";
-Console.WriteLine("Введите целое число №3 и нажмите enter");
+Console.WriteLine("Введите целое число №3 и нажмите enter: ");
 string InputC = Console.ReadLine() ?? "";
 
 int a = Convert.ToInt32(InputA);
--- Task_6/Program.cs
+++ Task_6/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт,
 // является ли число чётным (делится ли оно на два без остатка).
 
-Console.WriteLine("Введите число и нажмите enter");
+Console.WriteLine("Введите целое число и нажмите enter: ");
 string inputA = Console.ReadLine() ?? "";
 double a = Convert.ToDouble(inputA);
 //double res = a%2;
