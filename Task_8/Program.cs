1cc2f81d9e2bd358c65baad450427b2043929cac
VeraBu <opilki-in-yhe-head@mail.ru>
2022-11-14 15:04:43 +0300

    add Task 8 and Task 8_1

---
+++ Task_8/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе
+// показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите целое число и нажмите enter: ");
+string inputN = Console.ReadLine() ?? "";
+double numberN = Convert.ToDouble(inputN);
+int interim = 1;
+
+Console.Write($" {numberN} -> ");
+while (interim <= numberN)
+{
+    if (interim%2==0)
+    {
+        Console.Write($" {interim}, ");
+    }
+    interim=interim+1;
+}
---
+++ Task_8_1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе
+// показывает все чётные числа от 1 до N.
+// Вариант, в котором прогамма "проходит мимо нечетных", правда начало с 2, а не с 1
+
+Console.WriteLine("Введите целое число и нажмите enter: ");
+string inputN = Console.ReadLine() ?? "";
+double numberN = Convert.ToDouble(inputN);
+int interim = 2;
+
+Console.Write($" {numberN} -> ");
+while (interim <= numberN)
+{
+    Console.Write($" {interim}, ");
+    interim=interim+2;
+}
